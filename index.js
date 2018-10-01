0fe17f2a5eae95ad969b07248f1948845bbae440
Keenan <keenan@sider.review>
2018-10-01 19:27:37 +0900

    Stuff

--- index.js
+++ index.js
@@ -18,12 +18,14 @@ const findGenreDoubles = folder => {
   const doubled = []
   allPlaylists.map(playlist => {
     const tracks = getPlaylistTracks(playlist)
-    tracks.map(track => seenTracks.includes(track) ? doubled.push(track) : seenTracks.push(track))
+    tracks.map(track => seenTracks.find(seen => seen.name == track.name && seen.artist == track.artist) ? doubled.push(track) : seenTracks.push(track))
+    console.log(doubled)
   })
   return doubled
 }
 
 function main() {
+  console.log(findGenreDoubles("1. Genres"))
   // const albums = getPlaylistAlbums('Incomplete Albums')
   // albums.forEach(album => {
   //   const count = getAlbumTrackCount(album)
@@ -38,7 +40,6 @@ function main() {
   // genrePlaylists.forEach(setPlaylistGenre)
   // setPlaylistGenre('Uncategorized')
   // setPlaylistGenre('Undetermined')
-  console.log(findGenreDoubles("1. Genres"))
 
 }
 
